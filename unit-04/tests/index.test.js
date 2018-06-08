7eddd28b0b9b3e0947d440f8f92b2ba38f1faf94
Satana Charuwichitratana <micksatana@live.com>
2018-06-08 17:20:16 +0700

    Improve unit 4 lab 2 test case

--- unit-04/tests/index.test.js
+++ unit-04/tests/index.test.js
@@ -5,12 +5,7 @@ describe('index.html', () => {
     let browser, page;
 
     beforeAll(async () => {
-        browser = await puppeteer.launch({
-            headless: true,
-            // --no-sandbox option required to run test on Travis-CI
-            // If you don't need it, this can be removed
-            args: ['--no-sandbox']
-        });
+        browser = await puppeteer.launch({ headless: false });
         page = await browser.newPage();
         await page.goto('http://localhost:7777');
     });
@@ -20,6 +15,39 @@ describe('index.html', () => {
             await page.waitForSelector(tagName, { timeout: 100 });
         });
     }
+
+    it(`should have a link to page-2.html`, async () => {
+        const hrefs = await page.evaluate(() => {
+            const anchors = document.querySelectorAll('a');
+            return [].map.call(anchors, a => a.href);
+        });
+        const rs = hrefs.reduce((prev, href) => {
+            if (prev || /page-2.html$/.test(href)) {
+                return true;
+            } else {
+                return false;
+            }
+        }, false);
+
+        expect(rs).toBe(true);
+    });
+
+    afterAll(async () => {
+        await browser.close();
+    });
+});
+
+describe('page-2.html', () => {
+    let browser, page;
+
+    beforeAll(async () => {
+        browser = await puppeteer.launch({ headless: false });
+        page = await browser.newPage();
+        await page.goto('http://localhost:7777/page-2.html');
+    });
+    it(`should exists`, async () => {
+        await page.waitForSelector('body');
+    });
 
     afterAll(async () => {
         await browser.close();
